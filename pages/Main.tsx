040d2e590957c59b74408916d3621ad97f19fa6e
Joonmo You <joonmo.you@gmail.com>
2021-12-14 01:42:29 +0900

    Add style feature

--- pages/Main.tsx
+++ pages/Main.tsx
@@ -1,9 +1,18 @@
 import { useSession } from "next-auth/react";
 import { Provider } from "react-redux";
-import Header from "../src/components/Header";
+import styled from "@emotion/styled";
+import User from "../src/components/User";
 import MeetingScheduleContainer from "../src/container/MeetingScheduleContainer";
 import store from "../src/store";
 import BookingPanelContainer from "../src/container/BookingPanelContainer";
+
+const Header = styled.div`
+  min-height: 16rem;
+  padding: 0.5rem;
+  border-radius: 0 0 15px 15px;
+  background-color: #9c9eff;
+  box-shadow: rgba(0, 0, 0, 0.25) 0 0 7px 0, rgba(0, 0, 0, 0.22) 0 0 10px 0;
+`;
 
 export default function Main() {
   const { data } = useSession();
@@ -14,8 +23,10 @@ export default function Main() {
 
   return (
     <Provider store={store}>
-      <Header user={data.user} />
-      <BookingPanelContainer />
+      <Header>
+        <User user={data.user} />
+        <BookingPanelContainer />
+      </Header>
       <MeetingScheduleContainer />
     </Provider>
   );
--- src/components/MeetingRoomPicker.tsx
+++ src/components/MeetingRoomPicker.tsx
@@ -8,19 +8,22 @@ const Container = styled.div`
   flex-direction: row;
   flex-wrap: wrap;
   gap: 2vw;
+  min-height: 2em;
 `;
 
 const Room = styled.div<{ selected: boolean }>`
-  width: 30vw;
-  max-width: 20rem;
-  height: 2rem;
-  border-radius: 20px;
-  border: 1px solid;
-  background-color: ${({ selected }) =>
-    selected ? "lightgreen" : "lightgray"};
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 30vw;
+  max-width: 20rem;
+  height: 2rem;
+  border: 1px solid #333;
+  border-radius: 20px;
+  font-weight: ${({ selected }) => (selected ? 600 : 400)};
+  background-color: ${({ selected }) => (selected ? "#66FBD2" : "#F1EDFF")};
+  transition-property: background-color;
+  transition-duration: 0.3s;
 `;
 
 interface Props {
