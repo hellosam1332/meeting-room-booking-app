29e950034a6c469917cf2cd69de4b45fd35a1e54
허진석 <jinseog.hur@kurlycorp.com>
2022-01-17 09:38:08 +0900

    when access token is expired issue refresh token

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,60 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+const clientId = process.env.GOOGLE_CLIENT_ID ?? "";
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET ?? "";
+
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
+async function refreshAccessToken(token: any) {
+  try {
+    const url = `https://oauth2.googleapis.com/token?${new URLSearchParams({
+      client_id: clientId,
+      client_secret: clientSecret,
+      grant_type: "refresh_token",
+      refresh_token: token.refreshToken,
+    })}`;
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    });
+
+    const refreshedTokens = await response.json();
+
+    console.info("refreshedTokens", refreshedTokens);
+
+    if (!response.ok) {
+      return {
+        ...token,
+        error: "RefreshAccessTokenError",
+      };
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_at * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    };
+  } catch (error) {
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
+
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      clientId,
+      clientSecret,
       authorization: {
         params: {
           prompt: "consent",
@@ -19,19 +68,37 @@ export default NextAuth({
   ],
   secret: process.env.SECRET,
   callbacks: {
-    async jwt({ token, account }) {
-      if (account?.access_token) {
+    async jwt({ token, user, account }) {
+      console.info("account", account);
+
+      if (account?.access_token && account.expires_at && user) {
         return {
           ...token,
           accessToken: account.access_token,
+          accessTokenExpires: Date.now() + account.expires_at * 1000,
+          refreshToken: account.refresh_token,
+          user,
         };
       }
-      return token;
+
+      // Return previous token if the access token has not expired yet
+      // @ts-ignore
+      if (Date.now() < token.accessTokenExpires) {
+        return token;
+      }
+
+      // Access token has expired, try to update it
+      return refreshAccessToken(token);
     },
     async session({ session, token }) {
+      if (token.error) {
+        throw Error(`token error ${token.error}`);
+      }
+
       return {
         ...session,
         accessToken: token.accessToken,
+        error: token.error,
       };
     },
   },
